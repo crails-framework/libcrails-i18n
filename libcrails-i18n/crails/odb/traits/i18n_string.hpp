8688e1c65d53d55044af80b45ffe281384e19eb7
plaristote <michael@unetresgrossebite.com>
2026-09-18 16:55:50 +0200

    fix i18n::String ODB traits only working for pgsql backend

--- libcrails-i18n/crails/odb/traits/i18n_string.hpp
+++ libcrails-i18n/crails/odb/traits/i18n_string.hpp
@@ -1,50 +1,33 @@
 #pragma once
-#ifndef TWILIDOC
-# include <cstring>
-# include "../../i18n_string.hpp"
 
-namespace odb
-{
-  namespace pgsql
-  {
-    template<>
-    class value_traits<i18n::String, id_string>
-    {
-    public:
-      typedef i18n::String value_type;
-      typedef value_type query_type;
-      typedef details::buffer image_type;
+#include <crails/odb/backends.hpp>
+#define CRAILS_ODB_ID_STRING id_string
 
-      static void
-      set_value (value_type& i18n_string,
-                 const details::buffer& b,
-                 std::size_t n,
-                 bool is_null)
-      {
-        if (i18n_string.is_localized() && b.data()[0] == '{')
-          odb::pgsql::value_traits<DataTree, id_string>::set_value(i18n_string, b, n, is_null);
-        else
-          i18n_string = std::string_view(b.data(), n);
-      }
-
-      static void
-      set_image (details::buffer& b,
-                 std::size_t& n,
-                 bool& is_null,
-                 const value_type& i18n_string)
-      {
-        if (i18n_string.is_localized())
-          odb::pgsql::value_traits<DataTree, id_string>::set_image(b, n, is_null, i18n_string);
-        else
-        {
-          std::string as_string = i18n_string.to_string();
-
-          is_null = false;
-          n = as_string.size();
-          std::memcpy(b.data(), as_string.c_str(), n);
-        }
-      }
-    };
-  }
-}
+#ifdef CRAILS_ODB_WITH_PGSQL
+# include <odb/pgsql/traits.hxx>
+# define TRAITS_INCLUDE_SQL_BACKEND pgsql
+# include "i18n_string_impl.hpp"
+# undef TRAITS_INCLUDE_SQL_BACKEND
 #endif
+#ifdef CRAILS_ODB_WITH_MYSQL
+# include <odb/mysql/traits.hxx>
+# define TRAITS_INCLUDE_SQL_BACKEND mysql
+# include "i18n_string_impl.hpp"
+# undef TRAITS_INCLUDE_SQL_BACKEND
+#endif
+#ifdef CRAILS_ODB_WITH_ORACLE
+# include <odb/oracle/traits.hxx>
+# define TRAITS_INCLUDE_SQL_BACKEND oracle
+# include "i18n_string_impl.hpp"
+# undef TRAITS_INCLUDE_SQL_BACKEND
+#endif
+#ifdef CRAILS_ODB_WITH_SQLITE
+# include <odb/sqlite/traits.hxx>
+# define TRAITS_INCLUDE_SQL_BACKEND sqlite
+# undef CRAILS_ODB_ID_STRING
+# define CRAILS_ODB_ID_STRING id_text
+# include "i18n_string_impl.hpp"
+# undef TRAITS_INCLUDE_SQL_BACKEND
+#endif
+
+#undef CRAILS_ODB_ID_STRING
